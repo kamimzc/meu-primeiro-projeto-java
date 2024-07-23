f0a70b19d338d84249494e549a6b3196951bb3d1
kamila <kamilamaluzaceola04@gmail.com>
2024-07-23 12:13:01 -0300

    teste

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Boas Vindas Turma DIO!!");
+
+        System.out.println("teste");
     }
 }
